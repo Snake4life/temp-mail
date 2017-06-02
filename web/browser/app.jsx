50703953dd26f020b88e1f531664c1f2fbfee906
Mike Côté <mikecote1990@gmail.com>
2017-06-01 22:32:47 -0400

    Better tab toggle

--- web/browser/app.jsx
+++ web/browser/app.jsx
@@ -8,7 +8,6 @@ import React from 'react';
 import {render} from 'react-dom';
 
 import Home from './home.jsx';
-import About from './about.jsx';
 import NavbarComponent from './navbar.jsx';
 
 class App extends React.Component {
@@ -16,26 +15,17 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { MainComponent: Home };
-    this.onHomeClick = this.onHomeClick.bind(this);
-    this.onAboutClick = this.onAboutClick.bind(this);
+    this.setMainComponent = this.setMainComponent.bind(this);
   }
 
-  onHomeClick (e) {
-    this.setState({ MainComponent: Home });
-    e.preventDefault();
-    e.stopPropagation();
-  }
-
-  onAboutClick (e) {
-    this.setState({ MainComponent: About });
-    e.preventDefault();
-    e.stopPropagation();
+  setMainComponent (Component) {
+    this.setState({ MainComponent: Component });
   }
 
   render () {
     return (
       <div className="container-fluid">
-        <NavbarComponent onHomeClick={this.onHomeClick} onAboutClick={this.onAboutClick} />
+        <NavbarComponent setMainComponent={this.setMainComponent} />
         <this.state.MainComponent />
       </div>
     );
--- web/browser/navbar.jsx
+++ web/browser/navbar.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
+import Home from './home.jsx';
+import About from './about.jsx';
+
 class NavbarComponent extends React.Component {
 	constructor(props) {
     super(props);
-    this.onHomeClick = props.onHomeClick;
-		this.onAboutClick = props.onAboutClick;
+		this.state = { activeTab: 'home' };
+    this.setMainComponent = props.setMainComponent;
+		this.onHomeClick = this.onHomeClick.bind(this);
+		this.onAboutClick = this.onAboutClick.bind(this);
   }
+
+	onHomeClick (e) {
+		this.setState({ activeTab: 'home' });
+		this.setMainComponent(Home);
+		e.preventDefault();
+		e.stopPropagation();
+	}
+
+	onAboutClick (e) {
+		this.setState({ activeTab: 'about' });
+		this.setMainComponent(About);
+		e.preventDefault();
+		e.stopPropagation();
+	}
 
   render() {
     return (
@@ -17,10 +36,10 @@ class NavbarComponent extends React.Component {
 
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav mr-auto">
-						<li className="nav-item">
+						<li className={"nav-item " + (this.state.activeTab === 'home' ? 'active' : '')}>
 							<a className="nav-link" onClick={this.onHomeClick} href="#">Home</a>
 						</li>
-						<li className="nav-item">
+						<li className={"nav-item " + (this.state.activeTab === 'about' ? 'active' : '')}>
 							<a className="nav-link" onClick={this.onAboutClick} href="#">About</a>
 						</li>
 					</ul>
