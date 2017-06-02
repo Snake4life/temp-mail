f58f2be56d721e2cf772d8e5307836ed655a4ce2
Mike Côté <mikecote1990@gmail.com>
2017-06-01 22:21:17 -0400

    Add some pages in react

--- web/browser/app.jsx
+++ web/browser/app.jsx
@@ -7,14 +7,36 @@ require('./app.scss');
 import React from 'react';
 import {render} from 'react-dom';
 
+import Home from './home.jsx';
+import About from './about.jsx';
 import NavbarComponent from './navbar.jsx';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { MainComponent: Home };
+    this.onHomeClick = this.onHomeClick.bind(this);
+    this.onAboutClick = this.onAboutClick.bind(this);
+  }
+
+  onHomeClick (e) {
+    this.setState({ MainComponent: Home });
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  onAboutClick (e) {
+    this.setState({ MainComponent: About });
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
   render () {
     return (
       <div className="container-fluid">
-        <NavbarComponent />
-        <h1>Welcome!</h1>
+        <NavbarComponent onHomeClick={this.onHomeClick} onAboutClick={this.onAboutClick} />
+        <this.state.MainComponent />
       </div>
     );
   }
--- web/browser/navbar.jsx
+++ web/browser/navbar.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class NavbarComponent extends React.Component {
+	constructor(props) {
+    super(props);
+    this.onHomeClick = props.onHomeClick;
+		this.onAboutClick = props.onAboutClick;
+  }
+
   render() {
     return (
 			<nav className="navbar navbar-toggleable-md navbar-light bg-faded">
@@ -11,8 +17,11 @@ class NavbarComponent extends React.Component {
 
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav mr-auto">
-						<li className="nav-item active">
-							<a className="nav-link">Home <span className="sr-only">(current)</span></a>
+						<li className="nav-item">
+							<a className="nav-link" onClick={this.onHomeClick} href="#">Home</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" onClick={this.onAboutClick} href="#">About</a>
 						</li>
 					</ul>
 				</div>
